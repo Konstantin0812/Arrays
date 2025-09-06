d05ed2ce04888d9d71f3dd25868f5a713745513d
Konstantin0812 <titi081203@gmail.com>
2025-09-06 19:21:28 +0300

    Add files via upload

---
+++ Program – Копие.cs
@@ -0,0 +1,32 @@
+﻿namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days =
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int numberOfday = int.Parse(Console.ReadLine());
+
+            if (numberOfday >= 1 && numberOfday <= 7)
+            {
+                numberOfday--;
+                Console.WriteLine(days[numberOfday]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+        }
+    }
+}
